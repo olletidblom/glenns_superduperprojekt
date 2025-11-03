822cd2c2ca802713045264833ddc675b3038f0e6
Elias.GS <kesebol.elias@telia.com>
2025-11-03 15:13:40 +0100

    Co-authored-by: axel <Hbitz@users.noreply.github.com> Co-authored-by: Erotits <Erotits@users.noreply.github.com> Co-authored-by: Tibalomium <Tibalomium@users.noreply.github.com> Co-authored-by: Nak2477 <Nak2477@users.noreply.github.com>

--- libs/HTTP.h
+++ libs/HTTP.h
@@ -11,6 +11,7 @@ typedef enum {
     http_client_POST = 2, 
     http_client_header = 4,
     http_client_GET = 5,
+    http_client_parse_header = 6,
     http_client_dispose = 7,
 } HTTP_Status;
 
@@ -25,19 +26,24 @@ typedef enum{
     http_POST = 2,
 }HTTP_Method;
 
+/*
 typedef struct
 {
     char* message;
 }HTTP_Response;
+*/
 
 typedef struct 
 {
     smw_task* task;
     void (*callback)();
+    void* context;
     HTTP_Status status;
     HTTP_Method method;
     TCPClient* tcp_client;
-    HTTP_Response response;
+    char* HTTP_response;
+    char* url;
+    int response_code;
 
 }HTTPClient;
 
