f1f9561b67f4aee3ece356d799a09a87444fc30a
tibalomium <ninjaharry@gmail.com>
2025-11-10 15:44:36 +0100

    Added included in main.c for client-c

--- client-c/src/main.c
+++ client-c/src/main.c
@@ -1,25 +1,24 @@
 #include "../../libs/HTTP.h"
 #include "../../libs/TCPClient.h"
+#include "../../libs/utils.h"
 #include <netdb.h>
 #include <stdio.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define PORT 8080
 
-int main()
-{
-    smw_init();
-    HTTP_run(http_GET, HTTP_work);
+int main() {
+  smw_init();
+  HTTP_run(http_GET, HTTP_work);
 
-    while(smw_getTaskCount() > 0)
-    {
-        smw_work(SystemMonotonicMS());
-    }
+  while (smw_getTaskCount() > 0) {
+    smw_work(SystemMonotonicMS());
+  }
 
-    smw_dispose();
+  smw_dispose();
 
-    return 0;
+  return 0;
 }
